2c6de9fe4240bc559d4c976961bce599283664a2
Andres Gutierrez <andres@redbooth.com>
2016-04-27 11:14:28 +0200

    Store data in localStorage

--- src/event/index.js
+++ src/event/index.js
@@ -1,10 +1,25 @@
-import {createStore} from 'redux';
+import { createStore, compose } from 'redux';
+import persistState, { mergePersistedState } from 'redux-localstorage';
+import adapter from 'redux-localstorage/lib/adapters/localStorage';
+import filter from 'redux-localstorage-filter';
+import {wrapStore} from 'react-chrome-redux';
+
+import CONSTANTS from '../constants';
 import rootReducer from './reducers';
 
-import {wrapStore} from 'react-chrome-redux';
-import CONSTANTS from '../constants';
+const reducer = compose(
+  mergePersistedState()
+)(rootReducer);
 
-const store = createStore(rootReducer, {});
+const storage = compose(
+  filter('count')
+)(adapter(window.localStorage));
+
+const createPersistentStore = compose(
+  persistState(storage, 'tlk2-chrome-plugin')
+)(createStore);
+
+const store = createPersistentStore(reducer);
 
 wrapStore(store, {
   portName: CONSTANTS.CHROME_PORT,
